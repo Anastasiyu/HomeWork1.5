5dd5d820514eeb9c8dcce39dde087e29870fb4b2
Anastasiyu <asy.safiulina@mail.ru>
2022-08-31 18:53:52 +0300

    ДЗ задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { Task1();
     }
 
-    }
+    public static void Task1() {
+        int[] box = new int[]{1, 2, 3};
+        double[] arrayBox = new double[3];
+        arrayBox[0] = 1.12;
+        arrayBox[1] = 2.13;
+        arrayBox[2] = 3.14;
+        boolean[] arrayBool = {true, false, false};
+        for (int i = 0; i < arrayBox.length; i++) {
+            System.out.print(box[i] + ",");
+
+        }
+        System.out.print(box[arrayBox.length-1]);
+    }}
